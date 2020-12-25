a3a8f75d5c01b3a6fb1f5fe6338eb0f9fe566656
LAPTOP-NKGJ4DT9\Admin <sss.sgd26@gmail.com>
2020-12-25 09:15:08 -0500

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You Loose!: ");
+            }
+            else
+            {
+                Console.WriteLine("You Win!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
